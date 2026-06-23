9616ed2dec9bbc60b9d2cf1b16d252b2adc35155
hishamshanza <nusrathchelat@gmail.com>
2026-06-23 09:05:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ DEFAULT_AUTH_CHANNELS = [int(x) for x in environ.get("AUTH_CHANNEL", "").split()
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hishammon:hishammon@cluster0.2g7bqyf.mongodb.net/?appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Hello:Hello@cluster0.froodpm.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mn_files')
 DATABASE_URI2 = environ.get('DATABASE_URI2', "")
